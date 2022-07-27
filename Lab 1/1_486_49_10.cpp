920d7d3c4abc804bfdca23d35ec30cb28db6204c
sadia-haris-ringthy <109970583+sadia-haris-ringthy@users.noreply.github.com>
2022-07-27 16:10:07 +0600

    Add files via upload

---
+++ Lab 1/1_486_49_10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter your number:";
+    cin>>a;
+    if(a%2==0&&a>2)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+    getch();
+}
+
